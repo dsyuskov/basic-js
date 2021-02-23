462fd85aba73c650582fd822201bbeee0b954c73
dsyuskov <yuskovss@gmail.com>
2021-02-23 15:24:06 +0400

    feat: add solution transform-array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,46 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function transform(arr) {
+  const result = [];
+
+  let task = null;
+  let prev = null;
+
+  arr.forEach((item) => {
+    switch (item) {
+      case "--discard-next": {
+        task = "--discard-next";
+        break;
+      }
+      case "--discard-prev": {
+        result.pop();
+        break;
+      }
+      case "--double-next": {
+        task = "--double-next";
+        break;
+      }
+      case "--double-prev": {
+        result.push(prev);
+        break;
+      }
+      default: {
+        prev = item;
+        if (task === "--double-next") {
+          result.push(item);
+        }
+        if (task !== "--discard-next") {
+          result.push(item);
+        }
+        
+        if (task === "--discard-next") {
+          prev = null;
+          result.push(null)
+        }
+        task = null;
+      }
+    }
+  });
+  
+  return result.filter(item => item !== null);
+}
